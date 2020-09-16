848a0cf24e7fafd8147b810cb830dd4e69e444cb
Alan Shaw <alan@tableflip.io>
2020-09-16 16:38:41 +0100

    refactor: pass args as pointer

--- gen/suites/vm_violations/caller_validation.go
+++ gen/suites/vm_violations/caller_validation.go
@@ -9,14 +9,14 @@ import (
 	. "github.com/filecoin-project/test-vectors/gen/builders"
 )
 
-func callerValidation(args chaos.CallerValidationArgs, expectedCode exitcode.ExitCode) func(v *MessageVectorBuilder) {
+func callerValidation(args *chaos.CallerValidationArgs, expectedCode exitcode.ExitCode) func(v *MessageVectorBuilder) {
 	return func(v *MessageVectorBuilder) {
 		v.Messages.SetDefaults(GasLimit(1_000_000_000), GasPremium(1), GasFeeCap(200))
 
 		alice := v.Actors.Account(address.SECP256K1, abi.NewTokenAmount(1_000_000_000_000))
 		v.CommitPreconditions()
 
-		v.Messages.Raw(alice.ID, chaos.Address, chaos.MethodCallerValidation, MustSerialize(&args), Nonce(0), Value(big.Zero()))
+		v.Messages.Raw(alice.ID, chaos.Address, chaos.MethodCallerValidation, MustSerialize(args), Nonce(0), Value(big.Zero()))
 		v.CommitApplies()
 
 		v.Assert.EveryMessageResultSatisfies(ExitCode(expectedCode))
--- gen/suites/vm_violations/main.go
+++ gen/suites/vm_violations/main.go
@@ -25,7 +25,7 @@ func main() {
 				Desc:    "verifies that an actor that performs no caller validation fails",
 			},
 			Selector: map[string]string{"chaos_actor": "true"},
-			MessageFunc: callerValidation(chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchNone}, exitcode.SysErrorIllegalActor),
+			MessageFunc: callerValidation(&chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchNone}, exitcode.SysErrorIllegalActor),
 		},
 		&VectorDef{
 			Metadata: &Metadata{
@@ -34,7 +34,7 @@ func main() {
 				Desc:    "verifies that an actor that validates the caller twice fails",
 			},
 			Selector: map[string]string{"chaos_actor": "true"},
-			MessageFunc: callerValidation(chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchTwice}, exitcode.SysErrorIllegalActor),
+			MessageFunc: callerValidation(&chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchTwice}, exitcode.SysErrorIllegalActor),
 		},
 		&VectorDef{
 			Metadata: &Metadata{
@@ -43,7 +43,7 @@ func main() {
 				Desc:    "verifies that an actor that validates against a nil allowed address set fails",
 			},
 			Selector: map[string]string{"chaos_actor": "true"},
-			MessageFunc: callerValidation(chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchIsAddress}, exitcode.SysErrForbidden),
+			MessageFunc: callerValidation(&chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchIsAddress}, exitcode.SysErrForbidden),
 		},
 		&VectorDef{
 			Metadata: &Metadata{
@@ -52,7 +52,7 @@ func main() {
 				Desc:    "verifies that an actor that validates against an address set that does not include the caller addr fails",
 			},
 			Selector: map[string]string{"chaos_actor": "true"},
-			MessageFunc: callerValidation(chaos.CallerValidationArgs{
+			MessageFunc: callerValidation(&chaos.CallerValidationArgs{
 				Branch: chaos.CallerValidationBranchIsAddress,
 				// caller address will be a brand new account NOT the system actor address
 				Addrs: []address.Address{builtin.SystemActorAddr},
@@ -65,7 +65,7 @@ func main() {
 				Desc:    "verifies that an actor that validates against a nil allowed type set fails",
 			},
 			Selector: map[string]string{"chaos_actor": "true"},
-			MessageFunc: callerValidation(chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchIsType}, exitcode.SysErrForbidden),
+			MessageFunc: callerValidation(&chaos.CallerValidationArgs{Branch: chaos.CallerValidationBranchIsType}, exitcode.SysErrForbidden),
 		},
 		&VectorDef{
 			Metadata: &Metadata{
@@ -74,7 +74,7 @@ func main() {
 				Desc:    "verifies that an actor that validates against a actor type set that does not include the caller type",
 			},
 			Selector: map[string]string{"chaos_actor": "true"},
-			MessageFunc: callerValidation(chaos.CallerValidationArgs{
+			MessageFunc: callerValidation(&chaos.CallerValidationArgs{
 				Branch: chaos.CallerValidationBranchIsType,
 				// caller will be of type account actor NOT system actor
 				Types: []cid.Cid{builtin.SystemActorCodeID},
